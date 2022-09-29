d2a966198ceb8f284e1bd9c2873d681203f07390
C4r50nC <xichen059@gmail.com>
2022-09-29 23:14:33 +1000

    Implemented user registration

--- controllers/generalController.js
+++ controllers/generalController.js
@@ -4,12 +4,55 @@ const Trainee = require('../models/trainee')
 
 exports.register = async (req, res) => {
     try {
-        const id = req.user._id
         const body = req.body
-        const role = body.role
+
+        if (body.password !== body.confirmPassword) {
+            throw Error('Password is not same with confirm password');
+        }
+
+        if (body.role == 'Athelete') {
+            if (await Trainee.findOne({email: body.email})) {
+                throw Error('This email is already registered')
+            }
+
+            const newTrainee = new Trainee({
+                email: body.email,
+                password: await bcrypt.hash(body.password, 10),
+                firstName: body.firstName,
+                lastName: body.lastName,
+                age: body.age,
+                gender: body.gender,
+                bookings: []
+            })
+
+            await newTrainee.save()
+            req.flash('successfulMessage', 'Successfully Registered')
+            res.redirect('/signup')
+        } else if (body.role == 'Coach') {
+            if (await Coach.findOne({email: body.email})) {
+                throw Error('This email is already registered')
+            }
+
+            const newCoach = new Coach({
+                email: body.email,
+                password: await bcrypt.hash(body.password, 10),
+                firstName: body.firstName,
+                lastName: body.lastName,
+                age: body.age,
+                gender: body.gender,
+                price: 0,
+                address: 'None',
+                bookings: [],
+                timeSlot: []
+            })
+
+            await newCoach.save()
+            req.flash('successfulMessage', 'Successfully Registered')
+            res.redirect('/signup')
+        }
         
     } catch (err) {
-        req.flash('Register Error', err.message)
+        req.flash('Register Error', 'Register Failed')
         res.redirect('/signup')
     }
 }
