2551ab2420157da3e7fb0d20fefcf8f4d7f718eb
C4r50nC <xichen059@gmail.com>
2022-10-01 19:39:43 +1000

    Added passport related code to app

--- index.js
+++ index.js
@@ -2,6 +2,23 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+const passport = require('passport');
+const flash = require('express-flash');
+const session = require('express-session');
+
+require('./auth/passport')(passport);
+app.use(flash());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 900000 },
+  }),
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 require('./models/db').connectDb()
 require('./models/db').initUsers()
 app.listen(3000, () => {
