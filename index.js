9c56ee577118a03a4b8dce27683f334858353b67
Andy Low <andyjunchenglow@gmail.com>
2022-10-04 02:55:58 +1100

    Uncomment flash and session, added concurrently

--- index.js
+++ index.js
@@ -7,20 +7,20 @@ const PORT = process.env.PORT || 5000;
 
 
 //const passport = require('passport');
-//const flash = require("express-flash");
-//const session = require("express-session");
+const flash = require("express-flash");
+const session = require("express-session");
 
 //require('./auth/passport')(passport);
 
-// app.use(flash());
-// app.use(
-//   session({
-//     secret: process.env.SESSION_SECRET,
-//     resave: false,
-//     saveUninitialized: false,
-//     cookie: { maxAge: 900000 },
-//   })
-// );
+app.use(flash());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 900000 },
+  })
+);
 
 //app.use(passport.initialize());
 //app.use(passport.session());
