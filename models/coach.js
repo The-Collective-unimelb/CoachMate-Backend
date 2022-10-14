e6e8193518e524235289ae2cb0b04ef4f4a3951d
ChengYin <loh.chengyin@gmail.com>
2022-10-14 11:56:37 +1100

    update  coach schema

--- models/coach.js
+++ models/coach.js
@@ -1,60 +1,69 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 var CoachSchema = new Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+    enum: ["Male", "Female"],
+  },
+  sessionType: {
+    type: String,
+    required: true,
+    enum: ["Private", "Group"],
+  },
+  privatePrice: {
+    type: Number,
+    required: true,
+  },
+  groupPrice: {
+    type: Number,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  bookings: [
+    {
+      booking: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Booking",
+        required: false,
+      },
     },
-    password: {
-        type: String,
-        required: true
+  ],
+  timeSlots: [
+    {
+      timeSlot: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "TimeSlot",
+        required: false,
+      },
     },
-    firstName: {
-        type: String,
-        required: true
-    },
-    lastName: {
-        type: String,
-        required: true
-    },
-    age: {
-        type: Number,
-        required: true
-    },
-    gender: {
-        type: String,
-        required: true,
-        enum: ['Male', 'Female']
-    },
-    price: {
-        type: Number,
-        required: true
-    },
-    address: {
-        type: String,
-        required: true
-    },
-    bookings: [
-        {
-          booking: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Booking',
-            required: false,
-            },
-        },
-    ],
-    timeSlots: [
-        {
-            timeSlot: {
-                type: mongoose.Schema.Types.ObjectId,
-            ref: 'TimeSlot',
-            required: false,
-            }
-        }
-    ]
-})
+  ],
+});
 
-const Coach = mongoose.model('Coach', CoachSchema);
+const Coach = mongoose.model("Coach", CoachSchema);
 module.exports = Coach;
