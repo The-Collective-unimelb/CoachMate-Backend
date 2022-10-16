ff86faa6de757b080da47fcb0088f531eac0512d
shanyao0206 <100841845+shanyao0206@users.noreply.github.com>
2022-10-16 22:33:58 +1100

    changed user route to accommodate for the removal of the user model

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const user = require("../models/user");
+const user = require("../models/trainee");
 
 router.get("/", (req, res) => {
   user
