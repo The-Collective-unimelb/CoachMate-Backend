f17bcb337221fafde9e5e661d8acb83b40c9ca74
ChengYin <loh.chengyin@gmail.com>
2022-11-10 11:40:38 +1100

    Prompt user to set up (edit) their profile

--- coachmate-frontend/src/components/pages/CoachDashboard.js
+++ coachmate-frontend/src/components/pages/CoachDashboard.js
@@ -1,12 +1,22 @@
 import { Link, useNavigate } from "react-router-dom";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import classes from "./CoachDashboard.module.css";
 import pfp from "../../assets/pfp-blue.jpg";
 import axios from "axios";
 
 import { AuthContext } from "../../App";
 
+var baseUrl = process.env.BASE_URL || "http://localhost:5000";
+
+if (
+  process.env.NODE_ENV === "production" ||
+  process.env.NODE_ENV === "staging"
+) {
+  baseUrl = "https://coachmate-2022.herokuapp.com";
+}
+
 function CoachDashboard() {
+  const [profileReady, setProfileReady] = useState(false);
   const navigate = useNavigate();
   const ctx = useContext(AuthContext);
 
@@ -17,6 +27,29 @@ function CoachDashboard() {
     }
   }, [ctx.isLoggedIn, navigate]);
 
+  useEffect(() => {
+    axios
+      .get(baseUrl + `/coaches/getDetails`, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        console.log(res)
+        if (
+          res.data.aboutMe === "undefined" ||
+          res.data.groupPrice === "undefined" ||
+          res.data.privatePrice === "undefiend"
+        ) {
+          setProfileReady(false);
+        } else {
+          setProfileReady(true);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        console.log("fetch profile error");
+      });
+  });
+
   return (
     <div className={classes["vertical-flex"]}>
       <div className={classes.topbar}>
@@ -25,6 +58,7 @@ function CoachDashboard() {
         </Link>
       </div>
       <h1 className={classes.heading}>DASHBOARD</h1>
+      {!profileReady && <p>Set up your profile!</p>}
       <div className={classes["dashboard-grid-container"]}>
         <div className={classes["profile-column"]}>
           <div className={classes["profile-box"]}>
